8d7494d3024830518444869a5536c901f714fccc
ethan <songyue2@illinois.edu>
2018-04-25 12:14:11 -0500

    ahah

--- app/src/main/java/com/example/ethan/mp7_androidapp_finalproject/MainActivity.java
+++ app/src/main/java/com/example/ethan/mp7_androidapp_finalproject/MainActivity.java
@@ -11,9 +11,11 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -35,17 +37,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    // nytimes/ttle something like this
+    // enter query: return results from different sources ex:BBC NYtimes
+    // trys to throw out invalid input, return msg in toast
     void startArticle(String query) {
         try {
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
                     Request.Method.GET,
-                    "https://api.nytimes.com/svc/suggest/v1/timestags?api-key="
-                            + BuildConfig.API_KEY + "&query=" + query,
-                    new JSONObject(),
-                    new Response.Listener<JSONObject>() {
+                    "'https://newsapi.org/v2/everything?"
+                            + "q=" + query + "&"
+                            + "from=" + "" + "&"//calender
+                            + "sortBy=popularity&"
+                            + BuildConfig.API_KEY,
+                    new JSONArray(),
+                    new Response.Listener<JSONArray>() {
                         @Override
-                        public void onResponse(final JSONObject response) {
+                        public void onResponse(final JSONArray response) {
                             Log.d(TAG, response.toString());
+                            try {
+                                Log.d(TAG, response.toString(2));
+                                final TextView titles = findViewById(R.id.titles);
+                                titles.setText(response.toString());
+                            } catch (JSONException ignored) { }
                         }
                     }, new Response.ErrorListener() {
                 @Override
@@ -53,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
                     Log.e(TAG, error.toString());
                 }
             });
-            requestQueue.add(jsonObjectRequest);
+            requestQueue.add(jsonArrayRequest);
         } catch (Exception e) {
             e.printStackTrace();
         }
